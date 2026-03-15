318c7cb441fef9c825184227aa50f29314d42f5a
十二. <2795983512@qq.com>
2026-03-15 21:11:20 +0800

    Add files via upload

---
+++ model/OpenCV/图像处理.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+
+#Canny边缘检测
+"""
+    1）使用高斯滤波器，以平滑图像，滤除噪声
+    2）计算图像中每个像素点的提督强度和方向
+    3）应用非极大值抑制，以消除边缘检测带来的杂散相应
+    4）应用双阈值检测来确定真实和潜在的边缘
+    5）通过抑制孤立的弱边缘最终完成边缘检测
+"""
+
+img = cv2.imread('/Users/app/Desktop/Deep-Learning-Fundamentals-and-Frameworks-main/model/OpenCV/图像操作/car.png', cv2.IMREAD_GRAYSCALE)
+v1 = cv2.Canny(img,120,150)#minvalue,maxvalue,双阈值，如果min指定的比较小,边界比较宽，如果min指定的比较大，边界比较窄,max同理
+v2 = cv2.Canny(img,50,100)#如果整体比较高，边界将会明显，清晰，但是得到的边界信息会相对少
+
+res = np.hstack((v1,v2))
+cv2.imshow('img',res)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+#梯度计算--Sobel算子
+img = cv2.imread('/Users/app/Desktop/Deep-Learning-Fundamentals-and-Frameworks-main/model/OpenCV/图像操作/car.png', cv2.IMREAD_GRAYSCALE)
+sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)#img:图片,cv2.CV_64F:输出带负数的值，若这里填-1:默认负数截断为0，dx，dy谁为1则最后计算哪个方向的梯度，ksize：sobel算子的大小
+cv2.imshow('sobelx',sobelx)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+#问题：由于永远都是右边减左边，白到黑能显示，黑到白会显示负数，不显示，所以应该取绝对值
+sobelx = cv2.convertScaleAbs(sobelx)
+cv2.imshow('sobelx',sobelx)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+#分别计算x和y的梯度权重，然后再求和。不建议直接一起，因为会重影
+sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)#img:图片,cv2.CV_64F:输出带负数的值，若这里填-1:默认负数截断为0，dx，dy谁为1则最后计算哪个方向的梯度，ksize：sobel算子的大小
+sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)#img:图片,cv2.CV_64F:输出带负数的值，若这里填-1:默认负数截断为0，dx，dy谁为1则最后计算哪个方向的梯度，ksize：sobel算子的大小
+sobelxy = cv2.addWeighted(sobelx,0.5,sobely,0.5,0)#前两项为权重，后一项为偏置
+cv2.imshow('sobelxy',sobelxy)
+cv2.waitKey(0)
+
+
+#scharr算子，和sobel算子思想一样，但是每一项数值都变大
+#laplacian算子，对噪音点敏感，所以不会单独用
